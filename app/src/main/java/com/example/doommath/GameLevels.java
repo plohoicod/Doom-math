c4f56be011868c03f2773c12423f43f94e30c659
Egor Polivtsev <letsbattle@yandex.ru>
2019-08-20 18:30:54 +0300

    Level 1 begin

--- app/src/main/java/com/example/doommath/GameLevels.java
+++ app/src/main/java/com/example/doommath/GameLevels.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -23,6 +24,20 @@ public class GameLevels extends AppCompatActivity {
             public void onClick(View view) {
                 try {
                     Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e){
+
+                }
+            }
+        });
+
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
                     startActivity(intent);
                     finish();
                 } catch (Exception e){
---
+++ app/src/main/java/com/example/doommath/Level1.java
@@ -0,0 +1,14 @@
+package com.example.doommath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+    }
+}
