4861439b04a695905ff0b26d2adb3d680728c96c
Egor Polivtsev <letsbattle@yandex.ru>
2019-08-21 13:26:12 +0300

    Theme changes, text field added

--- app/src/main/java/com/example/doommath/Level1.java
+++ app/src/main/java/com/example/doommath/Level1.java
@@ -2,6 +2,7 @@ package com.example.doommath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Level1 extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+    }
+
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(Level1.this, GameLevels.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e){
+
+        }
     }
 }
